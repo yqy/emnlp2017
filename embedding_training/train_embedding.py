b05d752f3b371c41cd2b41e3e2624edaee670eaa
yqy <200805247@qq.com>
2017-03-08 10:55:25 +0800

    add train_embedding

--- embedding_training/train_embedding.py
+++ embedding_training/train_embedding.py
@@ -3,10 +3,15 @@ import sys
 import timeit
 import random
 import numpy
+random.seed(110)
 
 import neural
 from conf import *
 import wordAttr
+
+
+import cPickle
+sys.setrecursionlimit(1000000)
 
 def get_index(d,w):
     if w in d:
@@ -18,51 +23,66 @@ def main():
 
     embedding = []
     start_time = timeit.default_timer()
-   
-    print >> sys.stderr,"Read word list from embdding"     
 
-    word_dict = {}
-    word_file = open(args.embedding_file)
-    line = word_file.readline()
-    # word_dict[UNK] = 0
-    # embedding 第0维: UNK
-    index = 1 
-    while True:
-        line = word_file.readline()
-        if not line:break
-        line = line.strip().split(" ")[0]
-        word_dict[line] = index
-        index += 1
-    WA = wordAttr.wordAttrDict("./zh-attributes.data")
-
-    ## generate training data ##
-    training_instances = []
-    f = open(args.data)
-    word_type_calculate = {}
-    while True:
-        line = f.readline()
-        if not line:break
-        line = line.strip().split(" ")
-        if not len(line) == 5:
-            continue
-        center_word = line[2]
-        if not center_word in word_dict:
-            continue
-        y1,y2,y3 = WA.get_att(center_word)
-        word_type_calculate.setdefault((y1,y2,y3),0)
-        word_type_calculate[(y1,y2,y3)] += 1
-
-        y = word_dict[center_word]
-        #if (line[0] in word_dict) and (line[1] in word_dict) and (line[3] in word_dict) and (line[4] in word_dict):
-            #training_instances.append( ([word_dict[line[0]],word_dict[line[1]],word_dict[line[3]],word_dict[line[4]]],y,y1,y2,y3) )
-        training_instances.append( ([get_index(word_dict,line[0]),get_index(word_dict,line[1]), get_index(word_dict,line[3]), get_index(word_dict,line[4])],y,y1,y2,y3) )
-
-    print >> sys.stderr, "Generate totally",len(training_instances),"instances"
-    end_time = timeit.default_timer()
-    print >> sys.stderr,"Totally", end_time - start_time, "seconds!"
+    if os.path.isfile("./model/save_data"):
+        print >> sys.stderr,"Read from file ./model/save_data"
+        read_f = file('./model/save_data', 'rb') 
+        training_instances = cPickle.load(read_f)
+        read_f.close() 
+    else:
+        print >> sys.stderr,"Read word list from embdding"     
     
-    for k in sorted(word_type_calculate.keys(),key=lambda a:word_type_calculate[a], reverse = True):
-        print >> sys.stderr, k, word_type_calculate[k]
+        word_dict = {}
+        word_file = open(args.embedding_file)
+        line = word_file.readline()
+        # word_dict[UNK] = 0
+        # embedding 第0维: UNK
+        index = 1 
+        while True:
+            line = word_file.readline()
+            if not line:break
+            line = line.strip().split(" ")[0]
+            word_dict[line] = index
+            index += 1
+        WA = wordAttr.wordAttrDict("./zh-attributes.data")
+    
+        ## generate training data ##
+        training_instances = []
+        f = open(args.data)
+        word_type_calculate = {}
+        while True:
+            line = f.readline()
+            if not line:break
+            line = line.strip().split(" ")
+            if not len(line) == 5:
+                continue
+            center_word = line[2]
+            if not center_word in word_dict:
+                continue
+            y1,y2,y3 = WA.get_att(center_word)
+            word_type_calculate.setdefault((y1,y2,y3),0)
+            word_type_calculate[(y1,y2,y3)] += 1
+    
+            y = word_dict[center_word]
+            #if (line[0] in word_dict) and (line[1] in word_dict) and (line[3] in word_dict) and (line[4] in word_dict):
+                #training_instances.append( ([word_dict[line[0]],word_dict[line[1]],word_dict[line[3]],word_dict[line[4]]],y,y1,y2,y3) )
+            training_instances.append( ([get_index(word_dict,line[0]),get_index(word_dict,line[1]), get_index(word_dict,line[3]), get_index(word_dict,line[4])],y,y1,y2,y3) )
+    
+        print >> sys.stderr, "Generate totally",len(training_instances),"instances"
+        end_time = timeit.default_timer()
+        print >> sys.stderr,"Totally", end_time - start_time, "seconds!"
+        
+        for k in sorted(word_type_calculate.keys(),key=lambda a:word_type_calculate[a], reverse = True):
+            print >> sys.stderr, k, word_type_calculate[k]
+
+    if os.path.isfile("./model/neural_model"):
+        read_f = file('./model/neural_model', 'rb')
+        net = cPickle.load(read_f)
+        print >> sys.stderr,"Read model from ./model/neural_model"
+
+    else:    
+        print >> sys.stderr, "Building neural network"
+        net = neural.Net(args.embedding_dimention,100,len(word_dict),args.batch_size)    
 
     training_instances_batch = []
     for i in range(len(training_instances)/args.batch_size):
@@ -79,16 +99,16 @@ def main():
             y3l.append(y3)
         training_instances_batch.append((numpy.array(xl,dtype=numpy.int32),numpy.array(yl,dtype=numpy.int32),numpy.array(y1l,dtype=numpy.int32),numpy.array(y2l,dtype=numpy.int32),numpy.array(y3l,dtype=numpy.int32)) ) 
 
-    print >> sys.stderr, "Building neural network"
-    net = neural.Net(args.embedding_dimention,100,len(word_dict),args.batch_size)    
     random.shuffle(training_instances_batch)
 
     print >> sys.stderr, "Begin Train!"
     loss = 999999999999
     for i in range(args.echos):
+        start_time = timeit.default_timer()
         this_loss = 0.0
         for x,y,y1,y2,y3 in training_instances_batch:
             this_loss += net.train_step(x,y,y1,y2,y3,args.lr)[0]
+
         if this_loss  < loss:
             loss = this_loss
             embedding = net.show_para()
@@ -100,7 +120,6 @@ def main():
         end_time = timeit.default_timer()
         print >> sys.stderr,"One Epoch for: ", end_time - start_time, "seconds!"
         print >> sys.stderr,"Loss:",this_loss
-
 
 if __name__ == "__main__":
     main()
